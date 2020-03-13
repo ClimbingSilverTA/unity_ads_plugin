167e8dd9514ac0fa36b2d7370a1fa0d9a313d2ff
Jordan Holland <thisrememo@gmail.com>
2020-03-13 13:32:28 +1000

    Add Android implementation.

--- android/src/main/kotlin/com/throwjojo/unity_ads_plugin/UnityAdsPlugin.kt
+++ android/src/main/kotlin/com/throwjojo/unity_ads_plugin/UnityAdsPlugin.kt
@@ -1,7 +1,11 @@
 package com.throwjojo.unity_ads_plugin
 
+import android.app.Activity
 import androidx.annotation.NonNull;
+import com.unity3d.ads.UnityAds
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -9,12 +13,12 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** UnityAdsPlugin */
-public class UnityAdsPlugin: FlutterPlugin, MethodCallHandler {
+public class UnityAdsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "unity_ads_plugin")
     channel.setMethodCallHandler(UnityAdsPlugin());
   }
-
   // This static function is optional and equivalent to onAttachedToEngine. It supports the old
   // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
   // plugin registration via this function while apps migrate to use the new Android APIs
@@ -30,16 +34,43 @@ public class UnityAdsPlugin: FlutterPlugin, MethodCallHandler {
       val channel = MethodChannel(registrar.messenger(), "unity_ads_plugin")
       channel.setMethodCallHandler(UnityAdsPlugin())
     }
+
+    var mainActivity: Activity? = null
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "initialize") {
+      val gameId = call.argument<String>("game_id")
+      val testMode = call.argument<Boolean>("test_mode")
+
+      if (mainActivity != null) {
+        UnityAds.initialize(mainActivity, gameId, testMode!!)
+        result.success(true)
+      } else {
+        result.success(false)
+      }
+    } else if (call.method == "show") {
+      if (UnityAds.isInitialized() && UnityAds.isReady() && mainActivity != null) {
+        UnityAds.show(mainActivity)
+        result.success(true)
+      } else {
+        result.success(false)
+      }
     } else {
       result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {}
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    mainActivity = binding.activity
   }
+
+  override fun onDetachedFromActivity() {
+    mainActivity = null
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {}
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
 }
