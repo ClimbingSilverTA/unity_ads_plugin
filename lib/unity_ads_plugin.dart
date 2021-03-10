e2b28c17b76c4560d87702873fd66486b9680142
Jordan Holland <thisrememo@gmail.com>
2021-03-10 12:40:27 +1000

    Migrate to null-safety.

--- lib/unity_ads_plugin.dart
+++ lib/unity_ads_plugin.dart
@@ -3,23 +3,11 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class UnityAdsPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('unity_ads_plugin');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  static const MethodChannel _channel = const MethodChannel('unity_ads_plugin');
 
   static initialize(String gameId, {bool testMode = false}) async {
-    await _channel.invokeMethod('initialize', {
-      'game_id': gameId,
-      'test_mode': testMode
-    });
+    await _channel.invokeMethod('initialize', {'game_id': gameId, 'test_mode': testMode});
   }
 
-  static Future<bool> show() async {
-    return await _channel.invokeMethod('show');
-  }
-
+  static Future<bool> show() async => await _channel.invokeMethod('show') ?? false;
 }
